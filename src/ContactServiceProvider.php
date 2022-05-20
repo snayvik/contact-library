0cd181012337eeb34d1ad2ccbe07af7939a3c104
Vinod Patidar <vinod@snayvik.com>
2022-05-20 18:05:04 +0530

    load routes in boot method

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -6,11 +6,13 @@ use Illuminate\Support\ServiceProvider;
 class ContactServiceProvider extends ServiceProvider
 {
     public function boot(){
+
         $this->loadViewsFrom(__DIR__.'/views','contact');
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        
     }
 
     public function register()
     {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
     }
 }
